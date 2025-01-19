0cd362b5e6e6e29884bdca4eb01854cfb0fd9978
Barriella Wertheimer <82733729+barriellaw@users.noreply.github.com>
2025-01-19 13:06:19 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ document.getElementById("start-btn").addEventListener("click", function() {
     let totalSeconds = hours * 3600 + minutes * 60 + seconds;
 
     const timerDisplay = document.getElementById("timer-display");
-    interval = setInterval(function() {
+    timerInterval = setInterval(function() {
         if (totalSeconds <= 0) {
             clearInterval(timerInterval);
             timerDisplay.textContent = "00:00:00";
@@ -34,8 +34,26 @@ document.getElementById("stop-btn").addEventListener("click", function() {
     clearInterval(timerInterval);
 });
 
+let ringtone;
+
 function playRingtone() {
-    const ringtone = new Audio("./royalty_free_cancer.mp3");
+    if (!ringtone) {
+        ringtone = new Audio("./royalty_free_cancer.mp3");
+    }
+
+    ringtone.loop = true;
     ringtone.play();
-    alert("Time's up!");
+
+    setTimeout(() => {
+        if (confirm("Time's up! Do you want to stop the alarm?")) {
+            stopRingtone();
+        }
+    }, 100);
+}
+
+function stopRingtone() {
+    if (ringtone) {
+        ringtone.pause();
+        ringtone.currentTime = 0;
+    }
 }
